3def2fdd8c4505574cdc82bc6d861419ab3aa3c8
s.venka <s.venka@alpinepr.net>
2024-02-15 16:51:12 +0530

    context-api-1s-method

---
+++ miniContext/src/assets/components/Login.tsx
@@ -0,0 +1,28 @@
+import { useState, useContext } from "react"
+import UserContext from "../../Context/UserContext";
+
+
+function Login() {
+    const [userName, setUserName] = useState("");
+    const [passWord, setPassword] = useState("");
+
+    const {setUser} = useContext(UserContext)
+
+    const handleClick = (e: any) => {
+        e.preventDefault();
+        setUser({userName, passWord})
+    }
+    return(
+        <div>
+            <h2>Login</h2>
+            <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Dalll"/>
+            {" "}
+            <input type="text" value={passWord} onChange={(e) => setPassword(e.target.value)} placeholder="password"/>
+            <button onClick={handleClick}>
+                Submit
+            </button>
+        </div>
+    )
+}
+
+export default Login
---
+++ miniContext/src/assets/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react"
+import UserContext from "../../Context/UserContext"
+import UserContextProvider from "../../Context/UserContextProvider";
+
+
+function Profile() {
+
+    const {user} = useContext(UserContext);
+
+    if(!user) return <div>Please Login!!</div>
+    
+    return(
+        <UserContextProvider>
+            <h1>Welcome  {user ? user.userName: ""} {""} {user ? user.passWord: ""} </h1> 
+        </UserContextProvider>
+    )
+}
+
+export default Profile
